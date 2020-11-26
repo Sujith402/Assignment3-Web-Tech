768bea621080f94e35bf61d35ed3d23988b3f9a8
Sujith <sujith402@gmail.com>
2020-11-26 12:56:14 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Faster. Definit
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        { this.props.count > 0 && <h1>Messages: {this.props.count}</h1>}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const count = 0;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App count = {count}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
