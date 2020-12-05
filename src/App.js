da114102b14c9d774ce918e412ec6a01172d7718
Sujith <sujith402@gmail.com>
2020-12-05 12:34:40 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import './Styles/demo_style.css';
 import Header from './components/Header/header.js';
 import Body from './components/Body/body.js';
+import Footer from './components/Footer/footer.js'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <Header />
         <div className='background-break'></div>
         <Body />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Logo from '../../imgs/dummy-logo.png'
+import {FaFacebook, FaInstagram} from 'react-icons/fa'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <>
+        <footer id='footer'>
+          <ul className='footer-content'>
+            <div className='container'>
+                <li>
+                  <div className='img-container'>
+                    <img src={Logo} alt='logo'></img>
+                  </div>
+                  <p>
+                    <b>E-mail</b>: <a href='#'>kuttadopt@kuttadoptindia.org</a>
+                  </p>
+                  <h2> Follow us: </h2>
+                  <hr />
+                  <ul className='contact-icons'>
+                    <li><FaFacebook /></li>
+                    <li><FaInstagram /></li>
+                  </ul>
+                  <h3><a href='#'>Privacy Policy</a></h3>
+                  <h3><a href='#'>Terms and Conditions</a></h3>
+                  <h3><a href='#'>FAQs</a></h3>
+                </li>
+                <li>
+                  Contact Us:
+                  <p> Add in submit e-mail card here</p>
+                </li>
+            </div>
+          </ul>
+        </footer>
+      </>
+    )
+  }
+}
